fac120ae3ea4f5fad7081b3dae4fd8e3230d122a
samiulalim1 <47431474+samiulalim1@users.noreply.github.com>
2025-08-31 21:35:12 +0600

    Update script.js

--- docs/files/script.js
+++ docs/files/script.js
@@ -1,11 +1,9 @@
-// GitHub raw link to your installer
-const DOWNLOAD_URL = "https://raw.githubusercontent.com/USER/REPO/BRANCH/sql-backup.php";
+const DOWNLOAD_URL = "https://raw.githubusercontent.com/samiulalim1/sql-backup/refs/heads/main/src/installer/main.php";
 
 const btn = document.getElementById("downloadBtn");
 const btnText = btn.querySelector(".btn-text");
 
 async function downloadInstaller() {
-	// show loader
 	btn.disabled = true;
 	btn.innerHTML = '<div class="spinner-border spinner-border-sm me-2" role="status"></div>Downloading...';
 
@@ -26,11 +24,11 @@ async function downloadInstaller() {
 	} catch (e) {
 		alert("Download failed. Please check the link.");
 	}
-
-	// restore button
+	
 	btn.disabled = false;
 	btn.innerHTML = '<i class="bi bi-download me-2"></i><span class="btn-text">Download <strong>sql-backup.php</strong></span>';
 }
 
 btn.addEventListener("click", downloadInstaller);
 document.getElementById("year").textContent = new Date().getFullYear();
+
